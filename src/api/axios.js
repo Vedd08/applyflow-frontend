ea71d22d3bd746f104a4011d9814b15dce6222a2
Vedant Sonawane <sonawanevedant42@gmail.com>
2026-01-02 23:08:08 +0530

    Configure Netlify auto deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,24 +1,30 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://applyflow-backend-8edy.onrender.com/api'
   baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
+/* ================= TOKEN BY ROLE ================= */
 const getTokenByRole = () => {
   const path = window.location.pathname;
 
-  if (path.startsWith('/admin')) return localStorage.getItem('admin_token');
-  if (path.startsWith('/recruiter')) return localStorage.getItem('recruiter_token');
-  return localStorage.getItem('student_token');
+  if (path.startsWith('/admin')) return localStorage.getItem('token');
+  if (path.startsWith('/recruiter')) return localStorage.getItem('token');
+  if (path.startsWith('/student')) return localStorage.getItem('token');
+
+  return localStorage.getItem('token');
 };
 
-api.interceptors.request.use(config => {
-  const token = getTokenByRole();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+/* ================= INTERCEPTOR ================= */
+api.interceptors.request.use(
+  (config) => {
+    const token = getTokenByRole();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
